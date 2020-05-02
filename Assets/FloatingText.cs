9e344c93ca778cde68f5a8b3de6f827fa7627a8d
cmeyerhardt <cmeyerhardt@users.noreply.github.com>
2020-05-02 16:21:21 -0700

    Added floating text and TMP assets

---
+++ Assets/FloatingText.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class FloatingText : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI text = null;
+
+    public void GenerateText(string message, Color? color = null)
+    {
+        if (color != null)
+        {
+            text.color = (Color)color;
+        }
+        text.text = message;
+    }
+
+    public void DestroySelf()
+    {
+        Destroy(gameObject);
+    }
+}
